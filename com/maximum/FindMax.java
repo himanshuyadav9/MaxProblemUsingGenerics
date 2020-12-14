f5808117b9927a445893010eb88a2e4f5320d7eb
Himanshu <yadavhimanshu63@gmail.com>
2020-12-14 12:29:32 +0530

    Finding maximum

---
+++ com/maximum/FindMax.java
@@ -0,0 +1,27 @@
+package com.maximum;
+
+import java.util.Scanner;
+
+public class FindMax {
+	public static Integer max(Integer first, Integer second, Integer third) {
+		Integer max = first;
+		if (second.compareTo(max) > 0)
+			max = second;
+		if (third.compareTo(max) > 0)
+			max = third;
+		return max;
+	}
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		Integer[] intArray = new Integer[3];
+		System.out.println("Enter first number");
+		intArray[0] = scanner.nextInt();
+		System.out.println("Enter second number");
+		intArray[1] = scanner.nextInt();
+		System.out.println("Enter third number");
+		intArray[2] = scanner.nextInt();
+		System.out.println("Max value " + max(intArray[0], intArray[1], intArray[2]));
+	}
+
+}
---
+++ com/maximum/MaxTest.java
@@ -0,0 +1,17 @@
+package com.maximum;
+
+import static org.junit.Assert.*;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MaxTest {
+
+	@Test
+	public void givenMaxNumberAtFirstPosition_ReturnMaximumValueSame() {
+		FindMax maxValue=new FindMax();
+		Integer result =maxValue.max(9, 8, 3);
+		Assert.assertSame(9, result);
+	}
+
+}
