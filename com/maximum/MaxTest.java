f83ad14a38ddccb664a9293e2cd3a0839ccb1475
Himanshu <yadavhimanshu63@gmail.com>
2020-12-14 22:27:35 +0530

    Refactor -2

--- com/maximum/MaxTest.java
+++ com/maximum/MaxTest.java
@@ -8,7 +8,7 @@ import org.junit.Test;
 public class MaxTest {
 
 	@Test
-	public void givenMaxStringAtThirdPosition_ReturnMaximumValueSame() {
+	public void givenMaxNumber_ReturnMaximumValueSame() {
 		Integer first = 56;
         Integer second = 35;
         Integer third = 65;
@@ -16,4 +16,22 @@ public class MaxTest {
 		Integer result =maxValue.testMaximum(first, second ,third);
 		Assert.assertSame(65, result);
 	}
+	@Test
+		public void givenMaxFloatNumber_ReturnMaximumValueSame() {
+			Float first = 56.56f;
+	        Float second = 35.567f;
+	        Float third = 65.65f;
+			FindMax maxValue=new FindMax(first, second ,third);
+			Float result =maxValue.testMaximum(first, second ,third);
+			Assert.assertEquals(65.65f, result, 0.0f);		
+	}
+	@Test
+	public void givenMaxString_ReturnMaximumValueSame() {
+		String first = "apple";
+		String second = "peach";
+		String third = "banana";
+		FindMax maxValue=new FindMax(first, second ,third);
+		String result =maxValue.testMaximum(first, second ,third);
+		Assert.assertSame("peach", result);		
+}
 }
