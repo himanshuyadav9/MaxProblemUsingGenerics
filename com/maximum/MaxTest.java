04b05a4fad44152db336f6448827db2fa03f505c
Himanshu <yadavhimanshu63@gmail.com>
2020-12-14 15:46:27 +0530

    Max Float

--- com/maximum/MaxTest.java
+++ com/maximum/MaxTest.java
@@ -26,4 +26,11 @@ public class MaxTest {
 		Integer result =maxValue.max(9, 8, 13);
 		Assert.assertSame(13, result);
 	}
+	
+	@Test
+	public void givenMaxFloatNumberAtFirstPosition_ReturnMaximumValueSame() {
+		FindMax maxValue=new FindMax();
+		Float result =maxValue.max(9.12f, 8.21f, 3.214f);
+		Assert.assertEquals(9.12f, result, 0.0f);
+	}
 }
