42147c3e81ab2a3f3b43924423c50084e5b730c1
Himanshu <yadavhimanshu63@gmail.com>
2020-12-14 12:47:46 +0530

    Finding maximum

--- com/maximum/MaxTest.java
+++ com/maximum/MaxTest.java
@@ -13,5 +13,11 @@ public class MaxTest {
 		Integer result =maxValue.max(9, 8, 3);
 		Assert.assertSame(9, result);
 	}
-
+	
+	@Test
+	public void givenMaxNumberAtSecondPosition_ReturnMaximumValueSame() {
+		FindMax maxValue=new FindMax();
+		Integer result = maxValue.max(8,18, 3);
+		Assert.assertSame(18, result);
+	}
 }
